61bd189a5511d9defb801980c8a8aabab10f1f9e
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 19:40:13 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,13 @@
+
 <?php
-	$host = 'localhost';
-	$db = 'ucl_database';
-	$user = 'root';
-	$pass = '';
-	$charset = 'utf8';
-	
-	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-	$options = [PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-				PDO::ATTR_EMULATE_PREPARES => false
-	];
-	$pdo = new PDO($dsn, $user, $pass, $options);
+  $servername = "localhost";
+  $username = "azure";
+  $password = " ";
+  $dbname = "ucl_database";
+ 
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
 ?>
