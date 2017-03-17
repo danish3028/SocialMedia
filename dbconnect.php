bdcba954961ceddb59706400f4b57e8c532bfba3
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 19:31:42 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,24 +1,14 @@
-
 <?php
-$connectstr_dbhost = '';
-$connectstr_dbname = 'ucl_database';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
- 
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
- 
-define('DB_NAME', $connectstr_dbname);
-define('DB_USER', $connectstr_dbusername);
-define('DB_PASSWORD', $connectstr_dbpassword);
-define('DB_HOST', $connectstr_dbhost);
-
+	$host = 'localhost';
+	$db = 'ucl_database';
+	$user = 'root';
+	$pass = '';
+	$charset = 'utf8';
+	
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+	$options = [PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES => false
+	];
+	$pdo = new PDO($dsn, $user, $pass, $options);
 ?>
