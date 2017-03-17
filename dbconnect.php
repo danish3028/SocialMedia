c28d45fb84289b3cd2741134ad7c8c53f1cfab80
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 21:32:35 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,16 +1,32 @@
-
 <?php
-$host = "br-cdbr-azure-south-b.cloudapp.net";
-$username = "b94a5738ca9e3b";
-$password = "password";
-$db = "02ba1498";
+$connectstr_dbhost = '';
+$connectstr_dbname = 'ucl_database';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
 
-// Create connection
-$conn = mysqli_connect($host, $username, $password, $db);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
-echo "Connected successfully";
+
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+
 ?>
