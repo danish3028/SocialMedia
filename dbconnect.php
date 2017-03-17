42f40c5f514857f9b46fb62521a719cb8975b3a8
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 18:25:58 +0000

    Testing Connection strings

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,26 @@
-<?php
-	$host = 'br-cdbr-azure-south-b.cloudapp.net';
-	$db = 'ucl_database';
-	$user = 'b94a5738ca9e3b';
-	$pass = '02ba1498';
-	$charset = 'utf8';
-	
-	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-	$options = [PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-				PDO::ATTR_EMULATE_PREPARES => false
-	];
-	$pdo = new PDO($dsn, $user, $pass, $options);
+
+
+$host = "br-cdbr-azure-south-b.cloudapp.net";
+
+$username = "b94a5738ca9e3b";
+
+$password = "02ba1498";
+
+
+
+
+
+$conn = mysqli_connect($host, $username, $password);
+
+
+
+if (!$conn) {
+
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+
+echo "Connected successfully";
+
 ?>
+
