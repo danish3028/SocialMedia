6c57f486d761174828cd7e8184914b94dd2d36d1
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 18:44:14 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,28 +1,14 @@
 <?php
-
-$host = "localhost";
-
-$username = "root";
-
-$password = "";
-
-$db = "ucl_database";
-
-
-
-
-
-$conn = mysqli_connect($host, $db, $username, $password);
-
-
-
-if (!$conn) {
-
-    die("Connection failed: " . mysqli_connect_error());
-
-}
-
-echo "Connected successfully";
-
+	$host = 'br-cdbr-azure-south-b.cloudapp.net';
+	$db = 'ucl_database';
+	$user = 'b94a5738ca9e3b';
+	$pass = '02ba1498';
+	$charset = 'utf8';
+	
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+	$options = [PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES => false
+	];
+	$pdo = new PDO($dsn, $user, $pass, $options);
 ?>
-
