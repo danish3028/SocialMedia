9b5eb6a0ff951c24604890344c4969c65af35a91
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 22:39:31 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,33 +1,14 @@
 <?php
-
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-$pdo = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
-
-if (!$pdo) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($pdo) . PHP_EOL;
-
-mysqli_close($pdo);
-
+	$host = 'us-cdbr-azure-southcentral-f.cloudapp.net';
+	$db = 'ucl_database';
+	$user = 'b5244e346bc0c1';
+	$pass = 'c26f1ca0';
+	$charset = 'utf8';
+	
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+	$options = [PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES => false
+	];
+	$pdo = new PDO($dsn, $user, $pass, $options);
 ?>
