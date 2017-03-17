b5e70b442ccd9ef34f81b34931c2e1456361972f
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 21:52:40 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,3 +1,4 @@
+
 <?php
 $connectstr_dbhost = '';
 $connectstr_dbname = 'ucl_database';
@@ -15,9 +16,15 @@ foreach ($_SERVER as $key => $value) {
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+	
+	$options = [PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES => false
+	];
+	$pdo = new PDO($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
 
-if (!$link) {
+
+if (!$pdo) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
@@ -27,6 +34,6 @@ if (!$link) {
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
-mysqli_close($link);
+mysqli_close($pdo);
 
 ?>
