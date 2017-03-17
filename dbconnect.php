6940864783a4ec5949a8c6b53f203b832f1e70eb
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 18:58:45 +0000

    Azure connect

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,24 @@
+
 <?php
-	$host = 'br-cdbr-azure-south-b.cloudapp.net';
-	$db = 'ucl_database';
-	$user = 'b94a5738ca9e3b';
-	$pass = '02ba1498';
-	$charset = 'utf8';
-	
-	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-	$options = [PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-				PDO::ATTR_EMULATE_PREPARES => false
-	];
-	$pdo = new PDO($dsn, $user, $pass, $options);
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+ 
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+ 
+define('DB_NAME', $connectstr_dbname);
+define('DB_USER', $connectstr_dbusername);
+define('DB_PASSWORD', $connectstr_dbpassword);
+define('DB_HOST', $connectstr_dbhost);
+
 ?>
