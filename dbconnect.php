0b6a737d3b34c72363cd3d60856fd2c6125e4037
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 20:34:02 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -20,4 +20,11 @@ define('DB_USER', $connectstr_dbusername);
 define('DB_PASSWORD', $connectstr_dbpassword);
 define('DB_HOST', $connectstr_dbhost);
 
+$dsn = "mysql:host=DB_HOST;dbname=DB_NAME;"
+	$options = [PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES => false
+	];
+	$pdo = new PDO($dsn, DB_USER, DB_PASSWORD, $options);
+
 ?>
