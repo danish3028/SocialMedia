6470f273f6eacd766ed578bac5bc552051c6a5d2
danish3028 <zcabset@ucl.ac.uk>
2017-03-17 20:29:20 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -11,7 +11,6 @@ foreach ($_SERVER as $key => $value) {
     }
     
     $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
